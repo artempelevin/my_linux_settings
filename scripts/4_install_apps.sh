f55ed114dc5481d62e106709cd48ec1f9ee23a23
artempelevin <artempelevin@list.ru>
2023-01-05 15:57:32 +0300

    add script with my apps

---
+++ scripts/4_install_apps.sh
@@ -0,0 +1,35 @@
+apt install zsh --yes;						# Удобная командная оболочка для замены sh и bash
+apt install xorg --yes;						# X сервер
+apt install i3-wm --yes;					# Тайловый оконный менеджер
+apt install lightdm --yes;					# Менеджер входа в систему
+apt install picom --yes;					# Композитный менеджер (тени, анимации,эфеекты и т.д.)
+apt install polybar --yes;					# Легковесная панель задач
+apt install rofi --yes;						# Менеджер выбора приложений
+apt install kitty --yes;					# Эмулятор терминала
+apt install lxappearance --yes;				# Графическое приложения для найстроки GTK приложений
+apt install qt5ct --yes;					# Графическое приложения для найстроки Qt приложений
+apt install qt5-style-plugins --yes;		# Плагин, позволющий Qt приложениям выглядеть идентично GTK 2
+apt install pulseaudio --yes;				# Набор библиотек для корректной работы звука в системе
+apt install pavucontrol --yes;				# Графическое приложения для управления звуком в системе
+apt install redshift --yes;					# Управление подцветкой монитора в зависимости от времени суток
+apt install pcmanfm --yes;					# Файловый менеджер
+apt install firefox-esr --yes;				# Браузер
+apt install telegram-desktop --yes;			# Мессенджер
+apt install nitrogen --yes;					# Менеджер обоев рабочего стола
+apt install speedcrunch --yes;				# Удобный калькулятор
+apt install flameshot --yes;				# Инструмент для создания скриншотов
+apt install diodon --yes;					# Буфер обмена + значок в трее
+apt install mate-system-monitor --yes;		# Диспетчер задач
+apt install network-manager-gnome --yes;	# Набор библиотек для работы сети + значок в трее
+apt install unzip --yes;					# Консольная утилита для архивирования/разархивирования .zip файлов
+apt install engrampa --yes;					# Менеджер архивов
+apt install geany --yes;					# Мощный текстовый редактор
+apt install hardinfo --yes;					# Утилита для просмотра сведний о железе
+apt install eom --yes;						# Программа для просмотра изображений
+
+
+# Шрифты
+apt install ttf-ubuntu-font-family --yes;
+apt install fonts-jetbrains-mono --yes;
+apt install fonts-font-awesome --yes;
+apt install fonts-powerline --yes;
