ae68d2d2a74cf1c380653350a0681b2aee8af5be
artempelevin <artempelevin@list.ru>
2023-01-05 14:59:56 +0300

    add scripts

---
+++ scripts/2_set_source_list.sh
@@ -0,0 +1,8 @@
+echo deb http://deb.debian.org/debian/ bullseye main contrib non-free > /etc/apt/sources.list;
+echo deb-src http://deb.debian.org/debian/ bullseye main contrib non-free >> /etc/apt/sources.list;
+echo deb http://security.debian.org/debian-security bullseye-security main contrib non-free	>> /etc/apt/sources.list;
+echo deb-src http://security.debian.org/debian-security bullseye-security main contrib non-free >> /etc/apt/sources.list;
+echo deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free >> /etc/apt/sources.list;
+echo deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free >> /etc/apt/sources.list;
+apt update;
+apt upgrade;
