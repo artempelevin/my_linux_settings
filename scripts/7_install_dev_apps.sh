5f4edd54b44128555767ecf919e35d95acf29444
artempelevin <artempelevin@list.ru>
2023-02-15 23:56:45 +0300

    feat: Добавлены пакеты для разработки

---
+++ scripts/7_install_dev_apps.sh
@@ -0,0 +1,5 @@
+apt update;
+apt install build-essential --yes;    # Основные пакеты для сборки
+apt install docker.io --yes;          # Демон и клиент Docker
+apt install docker-compose --yes;     # Консольная утилита для управления множеством Docker контейнеров
+apt install docker-compose-plugin     # Поддержка compose в Docker CLI
