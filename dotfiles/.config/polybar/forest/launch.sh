0696da3561ae301771a61f6a07e23303ffc194e9
artempelevin <artempelevin@list.ru>
2023-02-18 22:51:06 +0300

    feat: Добавлен новая тема polybar (пока только скопирована чужая не Nord тема)

--- dotfiles/.config/polybar/forest/launch.sh
+++ dotfiles/.config/polybar/forest/launch.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/forest"
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -q main -c "$HOME/.config/polybar/forest/config.ini" &
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
